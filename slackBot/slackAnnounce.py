51f971f548e4f016ea649542f8b41a012e29a30e
srshearer <srshearer@gmail.com>
2017-11-06 12:41:55 -0800

    Import adjustment.
    Removed unnecessary 'as secrets' line in import.

--- slackBot/slackAnnounce.py
+++ slackBot/slackAnnounce.py
@@ -18,7 +18,7 @@ import argparse
 
 sys.path.insert(
     0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from slackBot import secrets as secrets
+from slackBot import secrets
 
 
 def parse_arguments():
