b98631edcff195ec8dfb8db231834363f0489577
Steven Shearer <sshearer@apple.com>
2017-12-11 14:29:28 -0800

    Continued debugging of slackAnnounce.py. Attempting to get scripts to play nice with PlexPy

--- slackBot/slackAnnounce.py
+++ slackBot/slackAnnounce.py
@@ -14,14 +14,14 @@ import os
 import sys
 import argparse
 import json
-try:
-    import requests
-except ImportError:
-    print 'Attempting to install requests module via pip...'
-    import pip
-    pip.main(['install', 'requests'])
-    print '\nPlease try running the script again.\n'
-    sys.exit(1)
+import requests
+
+# except ImportError:
+#     print 'Attempting to install requests module via pip...'
+#     import pip
+#     pip.main(['install', 'requests'])
+#     print '\nPlease try running the script again.\n'
+#     sys.exit(1)
 
 sys.path.insert(
     0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
