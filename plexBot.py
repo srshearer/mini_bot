70533bda6d1ade502263c7c2ef91ec69fd4a0537
Steven Shearer <sshearer@apple.com>
2017-12-13 09:23:11 -0800

    Re-organized and cleaned up code. Moved server connection to a function. Moved omdb query into Movie class.

--- plexBot.py
+++ plexBot.py
@@ -14,6 +14,7 @@ To do:
     - find link to movie trailer
     - get token based auth working
     - add file size to notification
+    - verify movie search results were actually added recently
 """
 
 import os
@@ -24,7 +25,6 @@ import argparse
 sys.path.insert(0, os.path.join(os.path.dirname(__file__),
                                 '/usr/local/lib/python2.7/site-packages'))
 import requests
-# from plexapi.server import PlexServer
 from plexapi.myplex import MyPlexAccount
 sys.path.insert(
     0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
@@ -45,12 +45,6 @@ def parse_arguments():
     parser.add_argument('-i', '--guid', dest='imdb_guid', metavar='<IMDb guid>',
                         required=True, action='store',
                         help='Find movie by IMDb guid.')
-    parser.add_argument('-u', '--user', dest='user', metavar='<username>',
-                        required=False, action='store',
-                        help='User to send the message as.')
-    parser.add_argument('--serverurl', dest='plex_server_url', metavar='<url>',
-                        required=False, action='store',
-                        help='Override default Slack webhook url.')
     args = parser.parse_args()
     return args
 
@@ -77,7 +71,7 @@ class SlackArgsBundle(object):
 
 
 class Movie(object):
-    def __init__(self, imdb_guid, movie_info_plex, movie_info_omdb, defaults):
+    def __init__(self, imdb_guid, movie_info_plex, defaults):
         self.imdb_guid = imdb_guid
         self.color = defaults.color
 
@@ -87,15 +81,23 @@ class Movie(object):
         self._raw_duration = movie_info_plex.duration
         self.duration = conv_milisec_to_min(self._raw_duration)
         self.quality = get_duration(movie_info_plex)
-        self.add_date = str(movie_info_plex.addedAt)
         self._media_items = movie_info_plex.media
-        # self._raw_filesize = movie_info_plex.size
-        # self.filesize = convert_size(self._raw_filesize)
 
-        self.director = movie_info_omdb.director
-        self.writer = movie_info_omdb.writer
-        self.plot = movie_info_omdb.plot
-        self.poster_link = movie_info_omdb.poster_link
+        self._omdb_json = self._get_omdb_info(imdb_guid, defaults.omdb_key)
+        self.plot = self._omdb_json['Plot']
+        self.poster_link = self._omdb_json['Poster']
+        self.rating = self._omdb_json['Rated']
+        self.director = self._omdb_json['Director']
+        self.writer = self._omdb_json['Writer']
+        self.revenue = self._omdb_json['BoxOffice']
+        self.duration = self._omdb_json['Runtime']
+
+    def _get_omdb_info(self, imdb_guid, omdb_key):
+        _omdb_info_json = get_omdb_info(imdb_guid, omdb_key)
+        print _omdb_info_json
+        _omdb_info_data = json.loads(_omdb_info_json)
+        print _omdb_info_data
+        return _omdb_info_data
 
     @property
     def _raw_quality(self):
@@ -105,24 +107,12 @@ class Movie(object):
         return self.quality
 
     @property
-    def new_movie_json_attachment(self):
-        json_attachment = get_new_movie_notification_json(self)
+    def json_attachment(self):
+        json_attachment = get_movie_notification_json(self)
         return json_attachment
 
 
-class Omdb_Movie_Info(object):
-    def __init__(self, omdb_json):
-        self.data = json.loads(omdb_json)
-        self.plot = self.data['Plot']
-        self.poster_link = self.data['Poster']
-        self.rating = self.data['Rated']
-        self.director = self.data['Director']
-        self.writer = self.data['Writer']
-        self.revenue = self.data['BoxOffice']
-        self.duration = self.data['Runtime']
-
-
-def get_new_movie_notification_json(movie):
+def get_movie_notification_json(movie):
     pretext = 'New Movie Available: '
     movie_title_year = '{} ({})'.format(movie.title, movie.year)
     fallback = '{} {}'.format(pretext, movie_title_year)
@@ -132,8 +122,6 @@ def get_new_movie_notification_json(movie):
     duration = movie.duration
     plot = movie.plot
     director = movie.director
-    # writer = movie.writer
-    # filesize = movie.filesize
     rating = movie.rating
     poster_link = movie.poster_link
 
@@ -149,6 +137,26 @@ def get_new_movie_notification_json(movie):
         "image_url": poster_link,
     }
     return json_attachments
+
+def get_server_instance(defaults):
+    user = defaults.user
+    password = defaults.pw
+    servername = defaults.plex_server_name
+    account = MyPlexAccount(user, password)
+    plex = account.resource(servername).connect()
+    return plex
+
+def search_plex(plex, imdb_guid):
+    movies = plex.library.section('Movies')
+    found_movies = []
+    for result in movies.search(guid=imdb_guid):
+        found_movies.append(result)
+    if len(found_movies) < 1:
+        print 'Error: Could not locate movie: {}'.format(imdb_guid)
+        sys.exit(1)
+    for video in found_movies:
+        print 'Found: {}'.format(video.title)
+    return video
 
 def querey_omdb(omdb_query_url):
     response = requests.get(omdb_query_url,
@@ -166,8 +174,15 @@ def get_omdb_info(imdb_guid, omdb_key):
     omdb_querey_url = 'http://www.omdbapi.com/?i='
     omdb_querey_url = omdb_querey_url + imdb_guid + '&plot=short&apikey=' + omdb_key
     omdb_json_result = querey_omdb(omdb_querey_url)
-    omdb_movie_info = Omdb_Movie_Info(omdb_json_result)
-    return omdb_movie_info
+    return omdb_json_result
+
+def get_duration(movie):
+    media_items = movie.media
+    for elem in media_items:
+        raw_quality = str(elem.videoResolution)
+        quality = format_quality(raw_quality)
+    if quality:
+        return quality
 
 def conv_milisec_to_min(miliseconds):
     s, remainder = divmod(miliseconds, 1000)
@@ -184,14 +199,6 @@ def convert_file_size(size_bytes):
    s = round(size_bytes / p, 2)
    return '{} {}'.format(s, size_name[i])
 
-def get_duration(movie):
-    media_items = movie.media
-    for elem in media_items:
-        raw_quality = str(elem.videoResolution)
-        quality = format_quality(raw_quality)
-    if quality:
-        return quality
-
 def format_quality(raw_quality):
     input_quality = str(raw_quality)
     known_qualities = ['1080', '720', '480']
@@ -201,61 +208,22 @@ def format_quality(raw_quality):
         quality = input_quality.upper()
     return str(quality)
 
-def search_plex(plex, imdb_guid):
-    movies = plex.library.section('Movies')
-    found_movies = []
-    for result in movies.search(guid=imdb_guid):
-        found_movies.append(result)
-    if len(found_movies) < 1:
-        print 'Error: Could not locate movie: {}'.format(imdb_guid)
-        sys.exit(1)
-    for video in found_movies:
-        print 'Found: {}'.format(video.title)
-    return video
-
-
 def main():
     defaults = DefaultsBundle()
     args = parse_arguments()
-
     debug, dryrun = slackAnnounce.get_debug_state(args, defaults)
-
-    ## TOKEN BASED AUTH
-    # token = defaults.token
-    # baseurl = defaults.plex_server_url
-    # plex = PlexServer(baseurl, token)
-
-    # PASSWORD AUTH
-    user = defaults.user
-    password = defaults.pw
-    servername = defaults.plex_server_name
-    account = MyPlexAccount(user, password)
-    plex = account.resource(servername).connect()
-
     imdb_guid = str(args.imdb_guid)
 
+    plex = get_server_instance(defaults)
     movie_info_plex = search_plex(plex, imdb_guid)
-    if not movie_info_plex:
-        print 'Error: Could not locate movie in Plex library: ' + imdb_guid
-        sys.exit(1)
-    movie_info_omdb = get_omdb_info(imdb_guid, defaults.omdb_key)
-    if not movie_info_omdb:
-        print 'Error: Could not locate movie in omdb: ' + imdb_guid
-        sys.exit(1)
+    new_movie = Movie(imdb_guid, movie_info_plex, defaults)
 
-    new_movie = Movie(imdb_guid, movie_info_plex, movie_info_omdb, defaults)
-    json_attachments = new_movie.new_movie_json_attachment
-
-    slack_variables = SlackArgsBundle(json_attachments, debug=debug, dryrun=dryrun)
+    slack_args = SlackArgsBundle(new_movie.json_attachment,
+                                 debug=debug, dryrun=dryrun)
     slack_defaults = slackAnnounce.DefaultsBundle()
-    message_obj = slackAnnounce.set_slack_message(slack_variables, slack_defaults)
+    message_obj = slackAnnounce.set_slack_message(slack_args, slack_defaults)
 
     slackAnnounce.post_message(message_obj)
-
-    # movie_section = plex.library.section('Movies')
-    # recently_added_movies_list = movie_section.recentlyAdded(maxresults=maxresults)
-    #
-    # print_info_for_results(recently_added_movies_list)
 
 if __name__ == '__main__':
     main()
