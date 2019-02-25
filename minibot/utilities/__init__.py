30f3a26b923a58e33c109ee4a2f181aa4ff6c2d5
Steven Shearer <sshearer@apple.com>
2019-02-24 19:03:24 -0800

    Moved log file

--- minibot/utilities/__init__.py
+++ minibot/utilities/__init__.py
@@ -3,6 +3,6 @@ from utilities import config
 from utilities.utils import Logger
 
 logfile = os.path.abspath(
-    os.path.join(os.path.dirname(__file__), config.LOG_FILE))
+    os.path.join(os.path.dirname(os.path.dirname(__file__)), config.LOG_FILE))
 logger = Logger(file_path=logfile, stdout=True)
 logger.info('logfile: {}'.format(logfile))
